a120cb3d465c5e2f04a6b8c1743a010fc0e198bd
Matthew Cabezon <matt.cabezon@gmail.com>
2017-01-30 02:23:20 -0500

    work in progress. got a lot showing

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 #My Portfolio
+
+##Hey there! Here's a little website about me. Take a look if you'd like.
+
+###Made With:
+* HTML/CSS
+* JavaScript
+* jQuery
+* Materialize
--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
 
+  $('.slide').hide(); //hide slide initially
+
+  $('.toggleMenu').click(function() {
+    console.log('hello');
+    if ($('.menu').is(':visible')) { // https://api.jquery.com/visible-selector/ when clicked, if menu is visible, hide it/show home
+      $('.menu').hide();
+      $('.homepage').show();
+    }
+    else { //hides slide and shows menu when clicked if menu not visible
+      $('.slide').hide();
+      $('.menu').show();
+      $('.homepage').hide();
+    }
+  });
+
+  $('.selected').click(function() { //when selected div clicked do...
+    $('.menu').hide(); //hide menu onclick
+    if($(this).hasClass('aboutMeBtn') === true) { //if btn has a class of aboutMeBtn, show aboutMe slide
+      $('.aboutMe').show();
+    } else if($(this).hasClass('projectsBtn') === true) { //if btn has a class of projectsBtn, show projects slide
+      $('.projects').show();
+    } else if($(this).hasClass('contactBtn') === true) { //if btn has a class of contactBtn, show contact slide
+      $('.contact').show();
+    }
+  });
+
 }); //ends ready function
