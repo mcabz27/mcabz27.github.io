6cc75aa396f3ba42c9b3654dbdb79e290a83183c
Matthew Cabezon <matt.cabezon@gmail.com>
2017-01-30 04:04:33 -0500

    mvp basics

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #My Portfolio
 
-##Hey there! Here's a little website about me. Take a look if you'd like.
+##Hey there! Here's a little website about me. Take a look if you'd like.(Link in description)
 
 ###Made With:
 * HTML/CSS
--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+  $('nav').addClass('teal darken-4');
   $('.slide').hide(); //hide slide initially
 
   $('.toggleMenu').click(function() {
@@ -7,11 +8,15 @@ $(document).ready(function(){
     if ($('.menu').is(':visible')) { // https://api.jquery.com/visible-selector/ when clicked, if menu is visible, hide it/show home
       $('.menu').hide();
       $('.homepage').show();
+      $('.toggleMenu').text('Open Menu');
     }
     else { //hides slide and shows menu when clicked if menu not visible
       $('.slide').hide();
       $('.menu').show();
       $('.homepage').hide();
+      $('nav').removeClass('lime accent-4');
+      $('nav').addClass('teal darken-4');
+      $('.toggleMenu').text('Close Menu');
     }
   });
 
@@ -19,10 +24,19 @@ $(document).ready(function(){
     $('.menu').hide(); //hide menu onclick
     if($(this).hasClass('aboutMeBtn') === true) { //if btn has a class of aboutMeBtn, show aboutMe slide
       $('.aboutMe').show();
+      $('.toggleMenu').text('Open Menu');
+      $('nav').removeClass('teal darken-4 light-blue darken-4 deep-purple'); //removes classes from nav to match up
+      $('nav').addClass('lime accent-4'); //gives nav bar same color as slide
     } else if($(this).hasClass('projectsBtn') === true) { //if btn has a class of projectsBtn, show projects slide
       $('.projects').show();
+      $('.toggleMenu').text('Open Menu');
+      $('nav').removeClass('teal darken-4 lime accent-4 deep-purple'); //removes classes from nav to match up
+      $('nav').addClass('light-blue darken-4'); //gives nav bar same color as slide
     } else if($(this).hasClass('contactBtn') === true) { //if btn has a class of contactBtn, show contact slide
       $('.contact').show();
+      $('.toggleMenu').text('Open Menu');
+      $('nav').removeClass('teal darken-4 lime accent-4 light-blue'); //removes classes from nav to match up
+      $('nav').addClass('deep-purple darken-4'); //gives nav bar same color as slide
     }
   });
 
