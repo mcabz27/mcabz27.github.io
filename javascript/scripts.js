bcfc655f54748f219dce2f45bb9712b5feffb852
Matthew Cabezon <matt.cabezon@gmail.com>
2017-01-30 05:05:16 -0500

    add comments

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     if ($('.menu').is(':visible')) { // https://api.jquery.com/visible-selector/ when clicked, if menu is visible, hide it/show home
       $('.menu').hide();
       $('.homepage').show();
-      $('.toggleMenu').text('Open Menu');
+      $('.toggleMenu').text('Open Menu');  //changed btn text to open
     }
     else { //hides slide and shows menu when clicked if menu not visible
       $('.slide').hide();
@@ -16,7 +16,7 @@ $(document).ready(function(){
       $('.homepage').hide();
       $('nav').removeClass('lime accent-4');
       $('nav').addClass('teal darken-4');
-      $('.toggleMenu').text('Close Menu');
+      $('.toggleMenu').text('Close Menu');  //changed btn text to close
     }
   });
 
@@ -24,17 +24,17 @@ $(document).ready(function(){
     $('.menu').hide(); //hide menu onclick
     if($(this).hasClass('aboutMeBtn') === true) { //if btn has a class of aboutMeBtn, show aboutMe slide
       $('.aboutMe').show();
-      $('.toggleMenu').text('Open Menu');
+      $('.toggleMenu').text('Open Menu');  //changed btn text to open
       $('nav').removeClass('teal darken-4 light-blue darken-4 deep-purple'); //removes classes from nav to match up
       $('nav').addClass('lime accent-4'); //gives nav bar same color as slide
     } else if($(this).hasClass('projectsBtn') === true) { //if btn has a class of projectsBtn, show projects slide
       $('.projects').show();
-      $('.toggleMenu').text('Open Menu');
+      $('.toggleMenu').text('Open Menu'); //changed btn text to open
       $('nav').removeClass('teal darken-4 lime accent-4 deep-purple'); //removes classes from nav to match up
       $('nav').addClass('light-blue darken-4'); //gives nav bar same color as slide
     } else if($(this).hasClass('contactBtn') === true) { //if btn has a class of contactBtn, show contact slide
       $('.contact').show();
-      $('.toggleMenu').text('Open Menu');
+      $('.toggleMenu').text('Open Menu');  //changed btn text to open
       $('nav').removeClass('teal darken-4 lime accent-4 light-blue'); //removes classes from nav to match up
       $('nav').addClass('deep-purple darken-4'); //gives nav bar same color as slide
     }
